d6c75771c767c4d2ae581180566e09b31f5116c8
dededarirahmadi <dededarirahmadi@gmail.com>
2021-05-06 22:26:54 +0700

    Initial commit

---
+++ app/src/main/java/com/example/learnwebsocket/model/Bitcoin.kt
@@ -0,0 +1,13 @@
+package com.example.learnwebsocket.model
+
+import com.squareup.moshi.JsonClass
+
+/**
+ * Created on : 06/05/21 | 22.24
+ * Author     : dededarirahmadi
+ * Name       : dededarirahmadi
+ * Email      : dededarirahmadi@gmail.com
+ */
+
+@JsonClass(generateAdapter = true)
+data class Bitcoin(val price: String?)
